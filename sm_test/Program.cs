107d4c5a2868f3e08baa625a819fcc1c2dc5b17c
Киселев Михаил В <mvkiselev@group.legion.ru>
2021-06-18 17:57:21 +0300

    Version 20210618 17:57

--- sm_test/Program.cs
+++ sm_test/Program.cs
@@ -20,21 +20,92 @@ namespace sm_test
                 this.parameters = parameters_string.Replace("(", "").Replace(")", "").Split(',');
             }
         }
-
-        static void EdgeProcess(Dictionary<String, Element> elements, Element edge)
+        class Point
         {
-            Element element = elements[edge.parameters[1]]; // edge loop
+            public double x;
+            public double y;
+            public double z;
+
+            public Point(double x, double y, double z)
+            {
+                this.x = x;
+                this.y = y;
+                this.z = z;
+            }
+        }
+        class Edge
+        {
+            public Point center;
+            public double radius;
+
+            public Point point1;
+            public Point point2;
+            public Point point3;
+            public Point point4;
+
+            public Edge(double x, double y, double z, double r)
+            {
+                this.center = new Point(x, y, z);
+                this.radius = r;
+
+                this.point1 = new Point(0, 0, 0);
+                this.point2 = new Point(0, 0, 0);
+                this.point3 = new Point(0, 0, 0);
+                this.point4 = new Point(0, 0, 0);
+            }
+        }
+        static Edge EdgeProcess(Dictionary<String, Element> elements, Element e)
+        {
+            Element element = elements[e.parameters[1]];    // edge loop
             element = elements[element.parameters[1]];      // oriented edge
             element = elements[element.parameters[3]];      // edge curve
             element = elements[element.parameters[3]];      // circle
             double radius = double.Parse(element.parameters[2]);
             element = elements[element.parameters[1]];      // axis placement
             element = elements[element.parameters[1]];      // cartesian point
-            double x = double.Parse(element.parameters[1]);
-            double y = double.Parse(element.parameters[2]);
-            double z = double.Parse(element.parameters[3]);
 
-            Console.WriteLine("Center: {0} {1} {2} Radius: {3}", x, y, z, radius);
+            Edge edge = new Edge(double.Parse(element.parameters[1]), double.Parse(element.parameters[2]), double.Parse(element.parameters[3]), radius);
+
+            edge.point1.x = edge.center.x - edge.radius;
+            edge.point1.y = edge.center.y;
+            edge.point1.z = edge.center.z;
+
+            edge.point2.x = edge.center.x + edge.radius;
+            edge.point2.y = edge.center.y;
+            edge.point2.z = edge.center.z;
+
+            edge.point3.x = edge.center.x;
+            edge.point3.y = edge.center.y - edge.radius;
+            edge.point3.z = edge.center.z;
+
+            edge.point4.x = edge.center.x;
+            edge.point4.y = edge.center.y + edge.radius;
+            edge.point4.z = edge.center.z;
+
+            return edge;
+        }
+        static void EdgesProcess(Dictionary<String, Element> elements, Element e1, Element e2)
+        {
+            Edge edge1 = EdgeProcess(elements, e1);
+            Edge edge2 = EdgeProcess(elements, e2);
+
+            Console.WriteLine("Edge 1");
+            Console.WriteLine("Point 1: {0} {1} {2}", edge1.point1.x, edge1.point1.y, edge1.point1.z);
+            Console.WriteLine("Point 2: {0} {1} {2}", edge1.point2.x, edge1.point2.y, edge1.point2.z);
+            Console.WriteLine("Point 3: {0} {1} {2}", edge1.point3.x, edge1.point3.y, edge1.point3.z);
+            Console.WriteLine("Point 4: {0} {1} {2}", edge1.point4.x, edge1.point4.y, edge1.point4.z);
+            Console.WriteLine("Center: {0} {1} {2}", edge1.center.x, edge1.center.y, edge1.center.z);
+            Console.WriteLine("Radius: {0}", edge1.radius);
+
+            Console.WriteLine("Edge 2");
+            Console.WriteLine("Point 1: {0} {1} {2}", edge2.point1.x, edge2.point1.y, edge2.point1.z);
+            Console.WriteLine("Point 2: {0} {1} {2}", edge2.point2.x, edge2.point2.y, edge2.point2.z);
+            Console.WriteLine("Point 3: {0} {1} {2}", edge2.point3.x, edge2.point3.y, edge2.point3.z);
+            Console.WriteLine("Point 4: {0} {1} {2}", edge2.point4.x, edge2.point4.y, edge2.point4.z);
+            Console.WriteLine("Center: {0} {1} {2}", edge2.center.x, edge2.center.y, edge2.center.z);
+            Console.WriteLine("Radius: {0}", edge2.radius);
+
+            Console.WriteLine("Height: {0}", Math.Abs(edge1.center.z - edge2.center.z));
         }
         static void ConeProcess(Dictionary<String, Element> elements)
         {
@@ -56,10 +127,9 @@ namespace sm_test
                         double angle  = double.Parse(canonical_surface.parameters[3]);
                         double height = double.Parse(canonical_surface.parameters[2]) * Math.Tan((Math.PI / 2.0) - angle);
 
-                        EdgeProcess(elements, edge1);
-                        EdgeProcess(elements, edge2);
+                        EdgesProcess(elements, edge1, edge2);
 
-                        Console.WriteLine("Angle: {0} Height: {1}", angle, height);
+                        //Console.WriteLine("Angle: {0} Height: {1}", angle, height);
 
                     }
                 }
@@ -76,7 +146,8 @@ namespace sm_test
                 Console.WriteLine("Введите имя файла");
 
                 String file_name = Console.ReadLine();
-                StreamReader sr = new StreamReader(file_name); // (@"D:\EDUCATION\SM\ппд 1х3х5 д0,3 с фасками.stp");
+                StreamReader sr = new StreamReader(@"D:\EDUCATION\SM\ппд 1х3х5 д0,3 с фасками.stp");
+                //StreamReader sr = new StreamReader(file_name); // (@"D:\EDUCATION\SM\ппд 1х3х5 д0,3 с фасками.stp");
                 line = sr.ReadLine();
 
                 while(line != null) {
